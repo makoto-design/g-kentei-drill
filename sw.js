9ef74b9166a18f1060b110a8f27aa96773d33659
makoto-design <mktnkgw@gmail.com>
2026-08-05 04:51:53 +0000

    講義に図を追加（画像認識タスク、相関、攻撃の分類）

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  *     変化が少なく、毎回取りに行く必要がない。
  * どちらもオフライン時はキャッシュから返すので、機内モードでも動く。
  */
-const CACHE_VERSION = "gken-202608050448";
+const CACHE_VERSION = "gken-202608050451";
 
 const PRECACHE = [
   "./",
