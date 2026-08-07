efdd404aacf43ad92f32f6dbe79d36ee87f802ec
makoto-design <mktnkgw@gmail.com>
2026-08-07 00:24:43 +0000

    M1-03 の追記を反映

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  *     変化が少なく、毎回取りに行く必要がない。
  * どちらもオフライン時はキャッシュから返すので、機内モードでも動く。
  */
-const CACHE_VERSION = "gken-e481edcf9f";
+const CACHE_VERSION = "gken-9134906481";
 
 const PRECACHE = [
   "./",
