e42c58457c98407976e0a6a2d3a1597856846fa1
makoto-design <mktnkgw@gmail.com>
2026-08-26 12:29:20 +0000

    カーネルの説明の書き直しを反映

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
    原本は g-kentei-study の quiz/*.js。
    更新したら sw.js の CACHE_VERSION を上げること。 */
 window.QUIZ_DATA = {
-  "generatedAt": "2026-08-14",
+  "generatedAt": "2026-08-26",
   "lessons": [
     {
       "id": "M1-01",
